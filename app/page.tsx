c0eb19db6774cbc97414677f0538cceb9822fe57
Rahaf Als <rah2fals@MacBook-Air-Rahaf.local>
2026-08-05 10:55:18 +0300

    force cache refresh for home posters

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const [moviePosters, setMoviePosters] = useState<string[]>([
 useEffect(() => {
   const API_KEY = "8f5e24fc60071c7126030efdc18a2e6d";
 
-  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`)
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&t=${Date.now()}`)
     .then((res) => res.json())
     .then((data) => {
       if (data.results && data.results.length > 0) {
